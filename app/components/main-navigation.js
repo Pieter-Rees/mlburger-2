fc87a0e6def1b7814bc380d417f169f8f4993210
Pieter Rees <pieter@pieterrees.nl>
2018-05-10 16:46:54 +0200

    added menu

--- app/components/main-navigation.js
+++ app/components/main-navigation.js
@@ -1,4 +1,14 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  menuButton: false,
+  showMenu: false,
+
+  actions: {
+    toggleMenu() {
+      this.toggleProperty('menuButton');
+      this.toggleProperty('showMenu');
+
+    }
+  }
 });
