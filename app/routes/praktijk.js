af15bb61e15d1a2254ebf8528692f945eda2ba8c
Pieter Rees <pieter@pieterrees.nl>
2018-07-02 20:41:40 +0200

    fixed scroll to top

--- app/routes/praktijk.js
+++ app/routes/praktijk.js
@@ -1,7 +1,8 @@
 import Route from '@ember/routing/route';
+import ScrollToTop from '../mixins/scroll-to-top';
 
-export default Route.extend({
+export default Route.extend(ScrollToTop, {
   model() {
     return this.store.findAll('praktijk');
-  },
+  }
 });
